573ab5694c33579952a71f0eef9db510b4f9bcc1
Martin Dalgaard <martin@ceromedia.dk>
2013-02-22 13:03:22 +0100

    2.0 Massive update
    FontAwesome for icons, css for baggrounds, now remembers visible-state per domain

--- js/showtitle.js
+++ js/showtitle.js
@@ -1,14 +1,13 @@
 var chrome_title_tag_last_change = 0;
-var chrome_title_tag_current_pos = '';
 var chrome_title_tag = {
 
   div: document.createElement('div'),
   
   create_markup: function() {
     this.div.id = "showtitlewrapper";
-    this.div.className = 'bottomright';
-    this.div.innerHTML = '<p><span id="showtitle-title"></span> <span id="showtitleremovelink" class="link" title="Hide this bar">x</span> <span class="link" id="showtitlemove" title="Move this bar">(m)</span></p>';
-    
+    this.div.className = "not-initialized";
+    this.div.innerHTML = '<p><span id="showtitle-title"></span> <span class="link" id="showtitlemove" title="Move this bar"><i class="icon-move"></i></span> <span id="showtitleremovelink" class="link" title="Hide this bar"><i class="icon-eye-open"></i><i class="icon-eye-close"></i></span></p>';
+    document.body.appendChild(this.div);
   },
   
   new_title: function() {
@@ -19,40 +18,33 @@ var chrome_title_tag = {
     }
     
     chrome_title_tag_last_change = ts;
-    
     chrome_title_tag.set_title();
         
+  },
+
+  delayed_title: function() {
+    setTimeout('chrome_title_tag.new_title()', 100);
   },
   
   set_title: function() {
     var htmltitle = htmlspecialchars(document.title);
     var titlelength = parseInt(document.title.length);
-    var lengthdiff = 75 - titlelength;
-    
-    if (lengthdiff == 0) {
-      lengthdiff = '';
-    } else if (lengthdiff > 0) {
-      lengthdiff = '(' + lengthdiff + ' less than 75)';
-    } else {
-      lengthdiff = '(' + (lengthdiff*-1) + ' more than 75)';
-    }
-    
-    lengthdiff = htmlspecialchars(lengthdiff);
     
     var showtitle_title = document.getElementById('showtitle-title');
-    showtitle_title.setAttribute('title', 'Length: ' + titlelength + ' chars ' + lengthdiff);
-    showtitle_title.innerHTML = htmltitle;
+    if (showtitle_title !== null) {
+        showtitle_title.setAttribute('title', 'Length: ' + titlelength + ' chars');
+        showtitle_title.innerHTML = htmltitle;
+    }
   },
   
-  display: function(settings) {
-    chrome_title_tag_current_pos = settings.position;
-    this.div.setAttribute("class", chrome_title_tag_current_pos);
-  
-    document.body.appendChild(this.div);
-  },
-  
-  hide: function() {
-    this.div.parentNode.removeChild(this.div);
+  toggle_hide: function() {
+    if (this.div.className.match(/\bhide\b/)) {
+        this.div.className = this.div.className.replace(/hide/g , '');
+        localStorage["showtitle-hide"] = "false";
+    } else {
+        this.div.className += ' hide';
+        localStorage["showtitle-hide"] = "true";
+    }
   },
   
   blacklist: function() {
@@ -61,18 +53,28 @@ var chrome_title_tag = {
   
   check_pos: function() {
     chrome.extension.sendRequest({type: "get", key: 'position'}, function(response) {
-      var showtitlewrapper = document.getElementById('showtitlewrapper');
-      showtitlewrapper.setAttribute("class", response);
+      chrome_title_tag.update_pos_class(response);
     });
+  },
+
+  update_pos_class: function(pos) {
+    var showtitlewrapper = document.getElementById('showtitlewrapper');
+    if (showtitlewrapper !== null) {
+        showtitlewrapper.className = pos;
+        if (localStorage["showtitle-hide"] == "true") {
+            showtitlewrapper.className += ' hide';
+        };
+    }
+    chrome.extension.sendRequest({type: "move", position: pos}, function(response) {});
   },
   
   add_handlers: function() {
     var this_obj = this;
     document.getElementById('showtitleremovelink').onclick = function () {
-      this_obj.hide();
+      this_obj.toggle_hide();
       return false;
     };
-    
+
     document.getElementById('showtitlemove').onclick = function () {
       chrome.extension.sendRequest({type: "get", key: 'position'}, function(response) {
         var new_pos = '';
@@ -87,50 +89,32 @@ var chrome_title_tag = {
           new_pos = 'bottom_right';
         }
         
-        chrome_title_tag_current_pos = new_pos;
-        this_obj.div.setAttribute("class", new_pos);
-        
-        chrome.extension.sendRequest({type: "move", position: new_pos}, function(response) {});
+        chrome.extension.sendRequest({type: "move", position: new_pos}, function(response) {
+            this_obj.check_pos();
+        });
+
       });
     
       
       
       return false;
     };
-    
-    
-    
+
   },
   
   init: function() {
     var this_obj = this;
     this.create_markup();
     
-    
     chrome.extension.sendRequest({type: "settings"}, function(response) {
-      if ( ! response.is_blacklisted) {
-        this_obj.display(response);
+        this_obj.update_pos_class(response.position)
         this_obj.set_title();
         this_obj.add_handlers();
         
-        document.addEventListener('DOMSubtreeModified', this_obj.new_title, 'false');
-        
+        document.addEventListener('DOMSubtreeModified', this_obj.delayed_title, 'false');
         setInterval("chrome_title_tag.check_pos()",5000);
-        
-      }
-      
-      if (response.move_help) {
-        document.getElementById('showtitlemove').onmouseover = function () {
-          alert('Now, you can move the Title Bar around. Just keep clicking the arrow to switch corners :-)');
-          document.getElementById('showtitlemove').onmouseover = function() {};
-          chrome.extension.sendRequest({type: "set", key: 'move_help', value: false}, function(response) {  });
-        };
-        
-      }
-      
-    });
-    
-  }
+    }); // sendRequest
+  } // init
   
 };
 
