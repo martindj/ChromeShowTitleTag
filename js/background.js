08ca51e218c08717b1c44f410e0231d93e0cb9a1
Martin Dalgaard <martin@ceromedia.dk>
2013-02-23 14:22:39 +0100

    Code cleanup and commenting
    Embarrassingly late :'(

--- js/background.js
+++ js/background.js
@@ -1,31 +1,27 @@
 var settings = {
-  get_position: function() {
-    return config.has('position') ? config.get('position') : 'bottom_right';
-  },
-  set_position: function(corner) {
-    corner = htmlspecialchars(corner);
-    config.set('position', corner);
-  }
-}
-
-
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-
-    if (request.type == "settings") {
-      sendResponse({ position: settings.get_position() });
-    } else if (request.type == "move") {
-      settings.set_position(request.position);
-      sendResponse({ position: settings.get_position() });
-    } else if (request.type == "set") {
-      config.set(request.key, request.value);
-      sendResponse( config.get(request.key) );
-    } else if (request.type == "get") {
-      sendResponse( settings.get_position() );
-    } else {
-      sendResponse({ error:'error' });
+    get_position: function () {
+        return config.has('position') ? config.get('position') : 'bottom_right';
+    },
+    set_position: function (corner) {
+        corner = htmlspecialchars(corner);
+        config.set('position', corner);
     }
-
-  });
-
-
+}
+chrome.extension.onRequest.addListener(
+function (request, sender, sendResponse) {
+    if (request.type == "move") {
+        settings.set_position(request.position);
+        sendResponse({
+            position: settings.get_position()
+        });
+    } else if (request.type == "set") {
+        config.set(request.key, request.value);
+        sendResponse(config.get(request.key));
+    } else if (request.type == "get") {
+        sendResponse(settings.get_position());
+    } else {
+        sendResponse({
+            error: 'error'
+        });
+    }
+});
--- js/showtitle.js
+++ js/showtitle.js
@@ -1,122 +1,136 @@
 var chrome_title_tag_last_change = 0;
+
 var chrome_title_tag = {
-
-  div: document.createElement('div'),
-  
-  create_markup: function() {
-    this.div.id = "showtitlewrapper";
-    this.div.className = "not-initialized";
-    this.div.innerHTML = '<p><span id="showtitle-title"></span> <span class="link" id="showtitlemove" title="Move this bar"><i class="icon-hand-down"></i><i class="icon-hand-up"></i><i class="icon-hand-right"></i><i class="icon-hand-left"></i></span> <span id="showtitleremovelink" class="link" title="Hide this bar"><i class="icon-eye-open"></i><i class="icon-eye-close"></i></span></p>';
-    document.body.appendChild(this.div);
-  },
-  
-  new_title: function() {
-    var ts = Math.round((new Date()).getTime() / 1000);
+    div: document.createElement('div'),
     
-    if (chrome_title_tag_last_change > ts-1) {
-      return false;
-    }
-    
-    chrome_title_tag_last_change = ts;
-    chrome_title_tag.set_title();
-        
-  },
-
-  delayed_title: function() {
-    setTimeout('chrome_title_tag.new_title()', 100);
-  },
-  
-  set_title: function() {
-    var htmltitle = htmlspecialchars(document.title);
-    var titlelength = parseInt(document.title.length);
-    
-    var showtitle_title = document.getElementById('showtitle-title');
-    if (showtitle_title !== null) {
-        showtitle_title.setAttribute('title', 'Length: ' + titlelength + ' chars');
-        showtitle_title.innerHTML = htmltitle;
-    }
-  },
-  
-  toggle_hide: function() {
-    if (this.div.className.match(/\bhide\b/)) {
-        this.div.className = this.div.className.replace(/hide/g , '');
-        localStorage["showtitle-hide"] = "false";
-    } else {
-        this.div.className += ' hide';
-        localStorage["showtitle-hide"] = "true";
-    }
-  },
-  
-  blacklist: function() {
-    chrome.extension.sendRequest({type: "blacklist", url:location.hostname}, function(response) { });
-  },
-  
-  check_pos: function() {
-    chrome.extension.sendRequest({type: "get", key: 'position'}, function(response) {
-      chrome_title_tag.update_pos_class(response);
-    });
-  },
-
-  update_pos_class: function(pos) {
-    var showtitlewrapper = document.getElementById('showtitlewrapper');
-    if (showtitlewrapper !== null) {
-        showtitlewrapper.className = pos;
-        if (localStorage["showtitle-hide"] == "true") {
-            showtitlewrapper.className += ' hide';
-        };
-    }
-    chrome.extension.sendRequest({type: "move", position: pos}, function(response) {});
-  },
-  
-  add_handlers: function() {
-    var this_obj = this;
-    document.getElementById('showtitleremovelink').onclick = function () {
-      this_obj.toggle_hide();
-      return false;
-    };
-
-    document.getElementById('showtitlemove').onclick = function () {
-      chrome.extension.sendRequest({type: "get", key: 'position'}, function(response) {
-        var new_pos = '';
-        
-        if (response == 'bottom_right') {
-          new_pos = 'bottom_left';
-        } else if (response == 'bottom_left') {
-          new_pos = 'top_left';
-        } else if (response == 'top_left') {
-          new_pos = 'top_right';
-        } else {
-          new_pos = 'bottom_right';
+    /**
+     * Updates the title text with the new title, unless it's been less than 1 second since last update
+     * @return (void)
+     */
+    set_title: function () {
+        var ts = Math.round((new Date()).getTime() / 1000); // unix timestamp in seconds
+        if (chrome_title_tag_last_change > ts - 1) {
+            return false;
         }
-        
-        chrome.extension.sendRequest({type: "move", position: new_pos}, function(response) {
-            this_obj.check_pos();
+        chrome_title_tag_last_change = ts;
+
+        var htmltitle = htmlspecialchars(document.title);
+        var titlelength = parseInt(document.title.length);
+        var showtitle_title = document.getElementById('showtitle-title');
+        if (showtitle_title !== null) {
+            showtitle_title.setAttribute('title', 'Length: ' + titlelength + ' chars');
+            showtitle_title.innerHTML = htmltitle;
+        }
+    },
+
+    /**
+     * Updates the title in 0.1 second. Reason for delay: Wait for the DOM to update correctly.
+     * @return (void)
+     */
+    set_title_delayed: function () {
+        setTimeout('chrome_title_tag.set_title()', 100);
+    },
+
+    /**
+     * Toggle whether the title box is shown or hidden. Stores the state in localStorage to remember state for each domain.
+     * @return (void)
+     */
+    toggle_hide: function () {
+        if (this.div.className.match(/\bhide\b/)) {
+            this.div.className = this.div.className.replace(/hide/g, '');
+            localStorage["showtitle-hide"] = "false";
+        } else {
+            this.div.className += ' hide';
+            localStorage["showtitle-hide"] = "true";
+        }
+    },
+
+    /**
+     * Get the position setting (global)
+     * @return (void)
+     */
+    get_position: function () {
+        chrome.extension.sendRequest({
+            type: "get",
+            key: 'position'
+        }, function (response) {
+            chrome_title_tag.set_position(response);
         });
+    },
 
-      });
-    
-      
-      
-      return false;
-    };
+    /**
+     * Move the title bar to the correct place and save the setting (global)
+     * @param {string} position     Where to put the title bar
+     * @return (void)
+     */
+    set_position: function (position) {
+        var showtitlewrapper = document.getElementById('showtitlewrapper');
+        if (showtitlewrapper !== null) {
+            showtitlewrapper.className = position;
+            if (localStorage["showtitle-hide"] == "true") {
+                showtitlewrapper.className += ' hide';
+            };
+        }
+        chrome.extension.sendRequest({
+            type: "move",
+            position: position
+        }, function (response) {});
+    },
 
-  },
-  
-  init: function() {
-    var this_obj = this;
-    this.create_markup();
-    
-    chrome.extension.sendRequest({type: "settings"}, function(response) {
-        this_obj.update_pos_class(response.position)
-        this_obj.set_title();
-        this_obj.add_handlers();
-        
-        document.addEventListener('DOMSubtreeModified', this_obj.delayed_title, 'false');
-        setInterval("chrome_title_tag.check_pos()",5000);
-    }); // sendRequest
-  } // init
-  
+    /**
+     * Add event handlers (click on buttons etc)
+     * @return (void)
+     */
+    add_handlers: function () {
+        var this_obj = this;
+        document.getElementById('showtitleremovelink').onclick = function () {
+            this_obj.toggle_hide();
+            return false;
+        };
+        document.getElementById('showtitlemove').onclick = function () {
+            chrome.extension.sendRequest({
+                type: "get",
+                key: 'position'
+            }, function (response) {
+                var new_pos = '';
+                if (response == 'bottom_right') {
+                    new_pos = 'bottom_left';
+                } else if (response == 'bottom_left') {
+                    new_pos = 'top_left';
+                } else if (response == 'top_left') {
+                    new_pos = 'top_right';
+                } else {
+                    new_pos = 'bottom_right';
+                }
+                chrome.extension.sendRequest({
+                    type: "move",
+                    position: new_pos
+                }, function (response) {
+                    this_obj.get_position();
+                });
+            });
+            return false;
+        };
+    },
+
+    /**
+     * Initialize the plugin
+     * @return (cake)
+     */
+    init: function () {
+        var this_obj = this;
+        this.div.id = "showtitlewrapper";
+        this.div.className = "not-initialized";
+        this.div.innerHTML = '<p><span id="showtitle-title"></span> <span class="link" id="showtitlemove" title="Move this bar"><i class="icon-hand-down"></i><i class="icon-hand-up"></i><i class="icon-hand-right"></i><i class="icon-hand-left"></i></span> <span id="showtitleremovelink" class="link" title="Hide this bar"><i class="icon-eye-open"></i><i class="icon-eye-close"></i></span></p>';
+        document.body.appendChild(this.div);
+
+        this.set_title();
+        this.get_position();
+        this.add_handlers();
+        document.addEventListener('DOMSubtreeModified', this.set_title_delayed, 'false');
+
+        setInterval("chrome_title_tag.get_position()", 2000);
+    }
 };
-
+/* Initialize the plugin */
 chrome_title_tag.init();
-
